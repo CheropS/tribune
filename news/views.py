eed68cc461550f0cc76b600818db3c0c205d083d
Sharon Cherop <sh.cherop@gmail.com>
2021-10-05 14:36:04 +0300

    rendering templates for past dates

--- news/views.py
+++ news/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 import datetime as dt
 from django.http import HttpResponse, Http404
 
@@ -7,29 +7,8 @@ def welcome(request):
     return render(request, 'welcome.html')
 
 def news_of_day(request):
-    date=dt.date.today()
-
-    #Function to convert date object to find exact day
-    day=convert_dates(date)
-    html=f'''
-        <html>
-             <body>
-                <h1> News for {day} {date.day}-{date.month}-{date.year}</h1>
-            </body>
-        </html>
-    '''
-    
-    return HttpResponse(html)
-
-def convert_dates(dates):
-    #Function that gets the weekday number for the date.
-    day_number=dt.date.weekday(dates)
-
-    days=['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
-
-    #Returning the actual day of the week
-    day=days[day_number]
-    return day 
+    date=dt.date.today()    
+    return render(request, 'all-news/today-news.html', {"date": date,})
 
 def past_days_news(request,past_date):
 
@@ -39,13 +18,8 @@ def past_days_news(request,past_date):
     except ValueError:
         # Raise 404 error when ValueError is thrown
         raise Http404()
+        assert False
 
-    day=convert_dates(date)
-    html = f'''
-        <html>
-            <body>
-                <h1>News for {day} {date.day}-{date.month}-{date.year}</h1>
-            </body>
-        </html>
-            '''
-    return HttpResponse(html)
+    if date == dt.date.today():
+        return redirect(news_of_day)
+    return render(request, 'all-news/past-news.html', {"date": date})
