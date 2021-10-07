854a2925c2c2dc21b005480a8c8ddd47af746edc
Sharon Cherop <sh.cherop@gmail.com>
2021-10-07 09:55:29 +0300

    adding images to articles

--- news/urls.py
+++ news/urls.py
@@ -1,4 +1,6 @@
 from django.conf.urls import url 
+from django.conf import settings
+from django.conf.urls.static import static
 from . import views
 
 urlpatterns=[
@@ -8,3 +10,5 @@ urlpatterns=[
     url('^search/', views.search_results, name='search_results')
 
 ]
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
