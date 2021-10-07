3a696280918c3d7b5f5c2542d95958db28ffd318
Sharon Cherop <sh.cherop@gmail.com>
2021-10-07 10:09:18 +0300

    adding single articles feature

--- news/urls.py
+++ news/urls.py
@@ -7,7 +7,9 @@ urlpatterns=[
     # url('^$',views.welcome,name = 'welcome'),
     url('^$',views.news_of_day,name='newsToday'),
     url('^archives/(\d{4}-\d{2}-\d{2})/$',views.past_days_news,name = 'pastNews'),
-    url('^search/', views.search_results, name='search_results')
+    url('^search/', views.search_results, name='search_results'),
+    url('^article/(\d+)',views.article,name ='article')
+
 
 ]
 if settings.DEBUG:
